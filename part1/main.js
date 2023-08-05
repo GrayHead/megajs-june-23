3f9027c0971ba6595de836506090a5df4afc9a63
GrayHead <ss.zhuravlov@gmail.com>
2023-08-05 16:28:14 +0300

    done

---
+++ part1/main.js
@@ -0,0 +1,102 @@
+// ctrl /
+// var x = 123; // number
+// console.log(x);
+// x = 'okten'; // string
+// console.log(x);
+//
+// let a = 10050;
+// console.log(a);
+// a = 'igtuqgiuwggsfj';
+// console.log(a);
+
+
+// {
+//     let x = 100;
+//
+//
+// }
+//
+// console.log(x);
+
+
+// ctrl -
+
+// const PI = 3.14;
+// PI = 100500;
+
+//
+// let foo;
+// console.log(foo);
+// foo = 123.56; // number
+// console.log(typeof foo, foo);
+// foo = ' hello okten';
+// console.log(typeof foo, foo);
+// // > < >= <= == !=  === !==
+// foo = 5 < 6;
+// console.log(typeof foo, foo);
+// //                 0     1         2
+// let arr = [100500, false, 'okten'];
+// console.log(typeof arr, arr);
+//
+// console.log(arr[2]);
+//
+//
+// let user1 = {
+//     id: 1,
+//     name: 'vasya',
+//     surname: 'pupkin',
+//     age: 31
+//
+// }
+//
+// console.log(typeof user1, user1);
+//
+// console.log(user1.id);
+// console.log(user1["id"]);
+
+// let num1 = 123;
+// console.log(typeof num1, num1);
+//
+// let str1 = num1 + '';
+// console.log(typeof str1, str1);
+//
+// console.log(100 + 200 + '');
+
+
+// let str2 = '100500jhsahgsaigd';
+// let num2 = +str2;
+// // let num2 = parseInt(str2);
+// console.log(num2);
+
+let num3 = -10096738;
+let booNum = !!num3;
+console.log(booNum);
+
+let str3 = '';
+let strBoo = !!str3;
+console.log(strBoo);
+
+console.log(!!null);
+console.log(!!undefined);
+console.log(!!0);
+console.log(!!'');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
